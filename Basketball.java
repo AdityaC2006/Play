4e69ecbc21b163fee1be8ad4a300d9f39d7b86fd
AdityaC2006 <82692095+AdityaC2006@users.noreply.github.com>
2022-05-03 11:12:21 -0700

    j

---
+++ Basketball.java
@@ -0,0 +1,8 @@
+public class Basketball extends Player{
+  private int the;
+
+  public Basketball(String name, int height, String team, int number, int the){
+    super(name, height, team, number);
+    this.the = the;
+  }
+}
